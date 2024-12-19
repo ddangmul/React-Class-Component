00cb29cd248fb9cce210cb8a0be47c12e84c9547
ddangmul <j.ddangmul@gmail.com>
2024-12-19 17:31:59 +0900

    User 컴포넌트 함수형에서 클래스형으로 변경

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,19 @@
-import classes from './User.module.css';
+import { Component } from "react";
+import classes from "./User.module.css";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends Component {
+  // class 예약어
+  // construnctor() {
+  //   // 메소드 - 초기화 작업이 필요없어서 삭제
+  // }
+  render() {
+    // render는 props를 받지 않음. this를 통해 모든 props에 접근 가능.
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
