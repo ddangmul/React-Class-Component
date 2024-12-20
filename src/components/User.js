7b2a4c0ee11d2e3278ea043567a741848deba438
ddangmul <j.ddangmul@gmail.com>
2024-12-20 15:57:14 +0900

    오류경계 설정

--- src/components/User.js
+++ src/components/User.js
@@ -1,19 +1,40 @@
-import { Component } from "react";
-import classes from "./User.module.css";
+// import { Component } from "react";
+// import classes from "./User.module.css";
+
+// class User extends Component {
+//   // class 예약어
+//   // construnctor() {
+//   //   // 메소드 - 초기화 작업이 필요없어서 삭제
+//   // }
+//   componentWillUnmount() {
+//     console.log("User will unmount!");
+//   }
+
+//   render() {
+//     // render는 props를 받지 않음. this를 통해 모든 props에 접근 가능.
+//     return <li className={classes.user}>{this.props.name}</li>;
+//   }
+// }
+
+// // const User = (props) => {
+// //   return <li className={classes.user}>{props.name}</li>;
+// // };
+
+// export default User;
+
+
+import { Component } from 'react';
+
+import classes from './User.module.css';
 
 class User extends Component {
-  // class 예약어
-  // construnctor() {
-  //   // 메소드 - 초기화 작업이 필요없어서 삭제
-  // }
   componentWillUnmount() {
-    console.log("User will unmount!");
+    console.log('User will unmount!');
   }
 
   render() {
-    // render는 props를 받지 않음. this를 통해 모든 props에 접근 가능.
     return <li className={classes.user}>{this.props.name}</li>;
-  }
+  } 
 }
 
 // const User = (props) => {
--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,15 +1,96 @@
-import { Fragment, Component } from "react";
+// import { Fragment, Component } from "react";
+
+// import Users from "./Users";
+// import classes from "./UserFinder.module.css";
+// import UsersContext from "../store/users-context";
+
+// // const DUMMY_USERS = [
+// //   { id: "u1", name: "Max" },
+// //   { id: "u2", name: "Manuel" },
+// //   { id: "u3", name: "Julie" },
+// // ];
+
+// class UserFinder extends Component {
+//   static contextType = UsersContext;
+
+//   constructor() {
+//     super();
+//     this.state = {
+//       filteredUsers: [],
+//       searchTerm: "",
+//     };
+//   }
+
+//   // HTTP 요청 보내기
+//   componentDidMount() {
+//     this.setState({ filteredUsers: this.contextType });
+//   }
+
+//   // useEffect 대체
+//   componentDidUpdate(prevProps, prevState) {
+//     // 상태변화로 리렌더링 시 호출
+//     if (prevState.searchTerm !== this.state.searchTerm) {
+//       // 의존성을 if문 조건검사로 대체
+//       this.setState({
+//         filteredUsers: this.contextType.filter((user) =>
+//           user.name.includes(this.state.searchTerm)
+//         ),
+//       });
+//     }
+//   }
+
+//   searchChangeHandler(event) {
+//     this.setState({ searchTerm: event.target.value });
+//   }
+
+//   render() {
+//     return (
+//       <Fragment>
+//         <div className={classes.finder}>
+//           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+//         </div>
+//         <Users users={this.state.filteredUsers} />
+//       </Fragment>
+//     );
+//   }
+// }
+
+// // const UserFinder = () => {
+// //   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+// //   const [searchTerm, setSearchTerm] = useState("");
+
+// //   useEffect(() => {
+// //     setFilteredUsers(
+// //       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+// //     );
+// //   }, [searchTerm]);
+
+// //   const searchChangeHandler = (event) => {
+// //     setSearchTerm(event.target.value);
+// //   };
+
+// //   return (
+// //     <Fragment>
+// //       <div className={classes.finder}>
+// //         <input type="search" onChange={searchChangeHandler} />
+// //       </div>
+// //       <Users users={filteredUsers} />
+// //     </Fragment>
+// //   );
+// // };
+
+// export default UserFinder;
+
+import { Fragment, useState, useEffect, Component } from "react";
 
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
-
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
+import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary.jsx";
 
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
     super();
     this.state = {
@@ -19,17 +100,14 @@ class UserFinder extends Component {
   }
 
   componentDidMount() {
-    // HTTP 요청 보내기
-    this.setState({ filteredUsers: DUMMY_USERS });
+    // Send http request...
+    this.setState({ filteredUsers: this.context.users });
   }
 
-  // useEffect 대체
   componentDidUpdate(prevProps, prevState) {
-    // 상태변화로 리렌더링 시 호출
     if (prevState.searchTerm !== this.state.searchTerm) {
-      // 의존성을 if문 조건검사로 대체
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
@@ -46,7 +124,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
@@ -54,7 +134,7 @@ class UserFinder extends Component {
 
 // const UserFinder = () => {
 //   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
-//   const [searchTerm, setSearchTerm] = useState("");
+//   const [searchTerm, setSearchTerm] = useState('');
 
 //   useEffect(() => {
 //     setFilteredUsers(
@@ -69,7 +149,7 @@ class UserFinder extends Component {
 //   return (
 //     <Fragment>
 //       <div className={classes.finder}>
-//         <input type="search" onChange={searchChangeHandler} />
+//         <input type='search' onChange={searchChangeHandler} />
 //       </div>
 //       <Users users={filteredUsers} />
 //     </Fragment>
